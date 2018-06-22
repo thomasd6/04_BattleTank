c37dec0e1cec36ddabd399b0f9797f116ed125bd
DESKTOP-7OGRKCA\Thomas <tmdickson66@gmail.com>
2018-06-22 11:11:36 -0400

    Track Throttles

--- BattleTank/Source/BattleTank/Private/Projectile.cpp
+++ BattleTank/Source/BattleTank/Private/Projectile.cpp
@@ -30,9 +30,7 @@ void AProjectile::Tick(float DeltaTime)
 }
 
 void AProjectile::LaunchProjectile(float Speed)
-{
-	auto Time = GetWorld()->GetTimeSeconds();
-	UE_LOG(LogTemp, Warning, TEXT("%f: Projectile Launched at Speed %f"), Time, Speed);
+{	
 	ProjectileMovement->SetVelocityInLocalSpace(FVector::ForwardVector * Speed);
 	ProjectileMovement->Activate();
 }
